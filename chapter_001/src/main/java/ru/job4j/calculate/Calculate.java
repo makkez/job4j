94717c17ffb728d48d69449955eb2fe008c2c0ba
makkez <mailto:metallmaster@bk.ru>
2018-08-28 20:53:44 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+ * Класс Calculate представляет простую программу
+ * @author makkez
+ * @since 26.08.2018
+ * @version 1.0 
+ */
 public class Calculate {
 
+	/**
+	 * Точка входа в программу,
+	 * вывод в консоль строку Hello World!
+	 * @param args Массив строковых аргументов
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
