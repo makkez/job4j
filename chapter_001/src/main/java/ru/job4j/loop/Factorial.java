8fa3e25856b0ea2beec935ae09a93a17e360c410
makkez <mailto:metallmaster@bk.ru>
2018-09-09 22:32:52 +0300

    5.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+/**
+ * Класс для вычисления факториала числа.
+ *
+ * @version 1.0
+ * @since 09.09.2018
+ * @author makkez
+ */
+public class Factorial {
+	
+	/**
+	 * Метод вычисляет факториал заданного числа.
+	 *
+	 * @param n Число, факториал которого нужно вычислить.
+	 * @return Вычисленный факториал числа, 
+	 * возврящает 1, если задан 0,
+	 * возврящает -1, если задано отрицательное число.
+	 */
+	public int calc(int n) {
+		int result = 1;
+
+		if (n < 0) {
+			result = -1;
+		} else {
+			for (int i = 1; i <= n; i++) {
+				result *= i;
+			}
+		}
+		
+		return result;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for Factorial.
+ * 
+ * @version 1.0
+ * @since 09.09.2018
+ * @author makkez
+ */
+public class FactorialTest {
+	
+	/**
+	 * Test for calc() when number > 0.
+	 */
+	@Test
+	public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+		Factorial factorial = new Factorial();
+		int result = factorial.calc(5);
+		assertThat(result, is(120));
+	}
+	
+	/**
+	 * Test for calc() when number = 0.
+	 */
+	@Test
+	public void whenCalculateFactorialForZeroThenOne() {
+		Factorial factorial = new Factorial();
+		int result = factorial.calc(0);
+		assertThat(result, is(1));
+	}
+	
+	/**
+	 * Test for calc() when number < 0.
+	 */
+	@Test
+	public void whenCalculateFactorialForMinusFiveThenMinusOne() {
+		Factorial factorial = new Factorial();
+		int result = factorial.calc(-5);
+		assertThat(result, is(-1));
+	}
+}
