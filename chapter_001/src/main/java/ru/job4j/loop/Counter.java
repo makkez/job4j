2f9ab68d8732b173d74d5abc89193213544306ff
makkez <mailto:metallmaster@bk.ru>
2018-09-11 19:51:20 +0300

    Refresh

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -18,13 +18,11 @@ public class Counter {
 	 */
 	public int add(int start, int finish) {
 		int count = 0;
-		
 		for (int i = start; i <= finish; i++) {
 			if (i % 2 == 0) {
 				count += i;
 			}
 		}
-		
 		return count;
 	}
 }
